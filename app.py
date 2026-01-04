ed9b52b511b6fb496c89a460d31f16a10fafbe8a
priyanshurajab2-alt <priyanshurajab2@gmail.com>
2026-01-04 20:48:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,7 @@ oauth.register(
     authorize_url='https://accounts.google.com/o/oauth2/auth',
     api_base_url='https://www.googleapis.com/oauth2/v1/',
     userinfo_endpoint='https://www.googleapis.com/oauth2/v2/userinfo',
+    jwks_uri='https://www.googleapis.com/oauth2/v3/certs',  # ← ADD THIS
     client_kwargs={'scope': 'openid email profile'}
 )
 
